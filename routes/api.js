02a16459759466f3e7b23a375327e3fedff97a9f
Joshua Vial <joshua@enspiral.com>
2016-04-29 15:43:23 +1200

    unauthenticated api

--- routes/api.js
+++ routes/api.js
@@ -1,16 +1,42 @@
 var router = require('express').Router()
 
+//resource called tweet
+//POST /tweets (tick)
+
+//GET /tweets (tick)
+//GET /tweets/:id
+
+//PUT /tweets/:id - replace whole object
+//PATCH /tweets/:id -replace part of an object
+//e.g.
+//{id: 7, tweet: 'hi', color: 'red'}
+//PUT /tweets/7 {color: 'blue'} => {id: 7, color: 'blue'}
+//PATCH /tweets/7 {color: 'blue'} => {id:7, tweet: 'hi', color: 'blue'}
+
+//DELETE /tweets/:id
+
+//make unauthenticated
 function generateApiRoutes(knex) {
   router.get('/tweets', function(req, res) {
-    if (!req.session.userId) {
-      res.send('permission denied')
-    } else {
-      knex.select().table('tweets')
-      .then(function(data) {
-        res.json(data)
-      })
-    }
+    knex.select().table('tweets')
+    .then(function(data) {
+      res.json(data)
+    })
   })
+
+  router.post('/tweets', function(req, res) {
+    knex('tweets').insert({
+      tweeted: req.body.tweet,
+      userId: req.session.userId
+    }).then(function(data) {
+      res.json(data)
+    })
+  })
+
+  router.get('/tweets/:id', function(req, res) { })
+  router.put('/tweets/:id', function(req, res) { })
+  router.patch('/tweets/:id', function(req, res) { })
+  router.delete('/tweets/:id', function(req, res) { })
 
   return router
 }
--- test/api.js
+++ test/api.js
@@ -1,5 +1,6 @@
 var request = require('supertest')
 var test = require('tape')
+var bcrypt = require('bcrypt')
 
 var knex = require('knex')({
   client: 'pg',
@@ -10,12 +11,23 @@ var knex = require('knex')({
 })
 var app = require('../app')(knex)
 
-test('/api/v1/tweets', function(t) {
-  request(app)
-    .get('/api/v1/tweets')
-    .end(function(err, res) {
-      t.equals(res.text, 'permission denied')
-      t.end()
+test('GET /api/v1/tweets', function(t) {
+  knex('tweets')
+    .insert({tweeted: 'hi', userId: 17})
+    .then(function(res) {
+      request(app)
+        .get('/api/v1/tweets')
+        .end(function(err, res) {
+          console.log(res.body)
+          t.equal(res.body[0].tweeted, 'hi')
+          knex('tweets').del().then(function() {
+            t.end()
+          })
+        })
     })
-
 })
+
+test('POST /api/v1/tweets', function(t) {
+  t.end()
+})
+
